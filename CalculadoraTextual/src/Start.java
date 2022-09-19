5d3fc91a2e34e367139122f9eacaf86110f50220
Waldo Araque <46325891+waldoaraque@users.noreply.github.com>
2022-09-20 00:03:42 +0200

    adding function to iterate calculator

--- CalculadoraTextual/src/Start.java
+++ CalculadoraTextual/src/Start.java
@@ -17,22 +17,124 @@ public class Start {
 		companyBanner();
 		getMenu();
 		handler();
-		
-		/*
-		Float resultado;
-		resultado = suma(3, 4);
-		System.out.print(resultado.toString());
-		*/
-	}
-		
-	public static Float suma(Float x, Float y) {
-	/*
-	 * Método para sumar
-	 */
-		return x + y;
-		
 	}
 	
+	public static void companyBanner() {
+		String banner = "                           Calculadora by                            \n"+
+	                    "   ____    _    _     ____ _   _ _        _  _____ ___  ____  \n"+
+				        "  / ___|  / \\  | |   / ___| | | | |      / \\|_   _/ _ \\|  _ \\ \n"+
+	                    " | |     / _ \\ | |  | |   | | | | |     / _ \\ | || | | | |_) |\n"+
+				        " | |___ / ___ \\| |__| |___| |_| | |___ / ___ \\| || |_| |  _ <  \n"+
+				        "  \\____/_/   \\_\\_____\\____|\\___/|_____/_/   \\_\\_| \\___/|_| \\_\\  S.A\n";
+		
+		System.out.println(banner);
+	}
+	
+	public static void getMenu() {
+	/*
+	 * 
+	 */
+		System.out.println("Introduzca una opción del menú:   ");
+		System.out.println("     1. Sumar                     ");
+		System.out.println("     2. Restar                    ");
+		System.out.println("     3. Multiplicar               ");
+		System.out.println("     4. Dividir                   ");
+		System.out.println("     5. Número mayor de 3 números ");
+		System.out.println("     6. Capicua                   ");
+		System.out.println("     0. Salir                    \n");
+		
+		System.out.print("Introduzca una opción: ");
+		option = capturaDatos.nextInt();
+	}
+	
+	public static void handler() {
+	/*
+	 * Método para imprimir mensaje y capturar datos
+	 */
+		switch(option) {
+		    case 0: // SALIR
+		    	System.out.println("Adiós! ");
+		    	break;
+			case 1: // SUMAR
+				
+				//System.out.println("A sumar! ");
+				a = captureNumber("primer", "sumar");
+				b = captureNumber("segundo", "sumar");
+				resultado = suma(a, b);
+				getResult("suma", resultado);
+				cleanConsole();
+				break;
+			case 2: // RESTAR
+				//System.out.println("A restar! ");
+				a = captureNumber("primer", "restar");
+				b = captureNumber("segundo", "restar");
+				resultado = resta(a, b);
+				getResult("resta", resultado);
+				cleanConsole();
+				break;
+			case 3: // MULTIPLICAR
+				//System.out.println("A multiplicar! ");
+				a = captureNumber("primer", "multiplicar");
+				b = captureNumber("segundo", "multiplicar");
+				resultado = multiplica(a, b);
+				getResult("multiplicación", resultado);
+				cleanConsole();
+				break;
+			case 4: // DIVIDIR
+				//System.out.println("A dividir! ");
+				a = captureNumber("primer", "dividir");
+				b = captureNumber("segundo", "dividir");
+				resultado = divide(a, b);
+				getResult("división", resultado);
+				cleanConsole();
+				break;
+			case 5: // MAYOR DE 3 NUMEROS
+				System.out.println("A calcular número mayor! ");
+				cleanConsole();
+				break;
+			case 6: // CAPICUA
+				System.out.println("A evaluar número! ");
+				cleanConsole();
+				break;
+			default: // OTRO 
+				System.err.println("Error :: Ha introducido una opción inválida.");
+				break;
+		}
+
+	}
+	
+	public static void cleanConsole() {
+		capturaDatos.nextLine();
+		companyBanner();
+		getMenu();
+		handler();
+	}
+	
+	public static Float captureNumber(String chance, String operation) {
+		Float number;
+		System.out.print("Introduzca el "+ chance +" número flotante a "+ operation +": ");
+		number = capturaDatos.nextFloat();
+		return number;
+	}
+	
+	public static void getResult(String operation, Float res) {
+		if (operation == "mayorde3") {
+			System.out.println("El número "+ res +" es el mayor de los 3 números\n");
+			capturaDatos.nextLine();
+		}
+		
+		System.out.println(" La "+ operation +" de "+ a.toString() +" y "+ b.toString() + " es: "+ res.toString()+"\n");
+		capturaDatos.nextLine();
+	}
+	
+	public static Float suma(Float x, Float y) {
+		/*
+		 * Método para sumar
+		 */
+			return x + y;
+			
+	}
+		
 	public static Float resta(Float x, Float y) {
 	/*
 	 * Método para restar
@@ -70,99 +172,4 @@ public class Start {
 	 * Método para evaluar un string capicua
 	 */
 	}
-	
-	public static void getMenu() {
-	/*
-	 * 
-	 */
-		System.out.println("Introduzca una opción del menú:   ");
-		System.out.println("     1. Sumar                     ");
-		System.out.println("     2. Restar                    ");
-		System.out.println("     3. Multiplicar               ");
-		System.out.println("     4. Dividir                   ");
-		System.out.println("     5. Número mayor de 3 números ");
-		System.out.println("     6. Capicua                   ");
-		System.out.println("     0. Salir                    \n");
-		
-		System.out.print("Introduzca una opción: ");
-		option = capturaDatos.nextInt();
-	}
-	
-	public static void companyBanner() {
-		String banner = "                           Calculadora by                            \n"+
-	                    "   ____    _    _     ____ _   _ _        _  _____ ___  ____  \n"+
-				        "  / ___|  / \\  | |   / ___| | | | |      / \\|_   _/ _ \\|  _ \\ \n"+
-	                    " | |     / _ \\ | |  | |   | | | | |     / _ \\ | || | | | |_) |\n"+
-				        " | |___ / ___ \\| |__| |___| |_| | |___ / ___ \\| || |_| |  _ <  \n"+
-				        "  \\____/_/   \\_\\_____\\____|\\___/|_____/_/   \\_\\_| \\___/|_| \\_\\  S.A\n";
-		
-		System.out.println(banner);
-	}
-	
-	public static void handler() {
-	/*
-	 * Método para imprimir mensaje y capturar datos
-	 */
-		switch(option) {
-		    case 0: // SALIR
-		    	System.out.println("Adiós! ");
-		    	break;
-			case 1: // SUMAR
-				
-				//System.out.println("A sumar! ");
-				a = captureNumber("primer", "sumar");
-				b = captureNumber("segundo", "sumar");
-				resultado = suma(a, b);
-				getResult("suma", resultado);
-				break;
-			case 2: // RESTAR
-				//System.out.println("A restar! ");
-				a = captureNumber("primer", "restar");
-				b = captureNumber("segundo", "restar");
-				resultado = resta(a, b);
-				getResult("resta", resultado);
-				break;
-			case 3: // MULTIPLICAR
-				//System.out.println("A multiplicar! ");
-				a = captureNumber("primer", "multiplicar");
-				b = captureNumber("segundo", "multiplicar");
-				resultado = multiplica(a, b);
-				getResult("multiplicación", resultado);
-				break;
-			case 4: // DIVIDIR
-				//System.out.println("A dividir! ");
-				a = captureNumber("primer", "dividir");
-				b = captureNumber("segundo", "dividir");
-				resultado = divide(a, b);
-				getResult("división", resultado);
-				break;
-			case 5: // MAYOR DE 3 NUMEROS
-				System.out.println("A calcular número mayor! ");
-				break;
-			case 6: // CAPICUA
-				System.out.println("A evaluar número! ");
-				break;
-			default: // OTRO 
-				System.err.println("Error :: Ha introducido una opción inválida.");
-				break;
-		}
-
-	}
-	
-	public static Float captureNumber(String chance, String operation) {
-		Float number;
-		System.out.print("Introduzca el "+ chance +" número flotante a "+ operation +": ");
-		number = capturaDatos.nextFloat();
-		return number;
-	}
-	
-	public static void getResult(String operation, Float res) {
-		if (operation == "mayorde3") {
-			System.out.println("El número "+ res +" es el mayor de los 3 números");
-		}
-		
-		System.out.println(" La "+ operation +" de "+ a.toString() +" y "+ b.toString() + " es: "+ res.toString());
-		
-	}
-
 }
