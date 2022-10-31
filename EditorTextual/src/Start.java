d8527aee7b14a08e988adff36b00d8d18dceec8e
Waldo Araque <46325891+waldoaraque@users.noreply.github.com>
2022-10-31 18:35:07 +0100

    adding functions to work on files

--- EditorTextual/src/Start.java
+++ EditorTextual/src/Start.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class Start {
 	
 	protected static final Scanner capturaDatos = new Scanner(System.in); 
+	protected static final String workdir = System.getProperty("user.dir");
 	protected static int option = 0;
-	protected static String file;
+	protected static File file;
+	protected static String fileName, miniOption;
 	
 	public static void main(String[] args) {
 	/*
 	 * Ejecución del programa
 	 */
+		
 		companyBanner();
 		getMenu();
 		handler();
@@ -32,7 +39,7 @@ public class Start {
 	 * Método para imprimir el menú las veces que se requiera
 	 */
 		try {
-			System.out.println("Introduzca una opción del menú:   ");
+			System.out.println("Introduzca una opción del menú:   \n");
 			System.out.println("     1. Crear un fichero   ");
 			System.out.println("     2. Editar un fichero  ");
 			System.out.println("     3. Borrar un fichero  ");
@@ -62,35 +69,32 @@ public class Start {
 			case 1: // CREAR FICHERO
 				
 				System.out.println("Introduzca el nombre del nuevo fichero: ");
-				file = capturaDatos.next();
-				//if() {
-					
-				//}
+				fileName = capturaDatos.next();
 				
-				System.out.println("fichero creado...");
+				createFile(fileName);
+				
 				capturaDatos.nextLine();
 				cleanConsole();
 				break;
 			case 2: // EDITAR FICHERO
 				System.out.println("Introduzca el nombre del fichero a editar: ");
-				file = capturaDatos.next();
+				fileName = capturaDatos.next();
+				updateFile(fileName);
 				
-				
-				System.out.println("fichero editado...");
 				capturaDatos.nextLine();
 				cleanConsole();
 				break;
 			case 3: // BORRAR FICHERO
 				System.out.println("Introduzca el nombre del fichero a borrar: ");
-				file = capturaDatos.next();
+				fileName = capturaDatos.next();
+				deleteFile(fileName);
 				
-				System.out.println("fichero editado...");
 				capturaDatos.nextLine();
 				cleanConsole();
 				break;
 			case 4: // CREAR CARPETA
 				System.out.println("Introduzca el nombre de la carpeta a crear: ");
-				file = capturaDatos.next();
+				fileName = capturaDatos.next();
 				
 				
 				System.out.println("carpeta creada...");
@@ -122,5 +126,78 @@ public class Start {
 		companyBanner();
 		getMenu();
 		handler();
+	}
+	
+	public static void createFile(String fileName) {
+	/*
+	 * Método para validar y crear fichero
+	 */
+		Path path = Paths.get(workdir, fileName);
+		
+		file = new File(path.toString());
+		
+		if(file.exists()) {
+			System.out.println("Fichero existente ¿Desea sobrescribirlo?");
+			miniOption = capturaDatos.next("(Si|No)$");
+			
+			if(miniOption.startsWith("Si")) {
+				System.out.println("creating filesystemccsadsfa");
+				//return file;
+			} else {
+				cleanConsole();
+			}
+		} else {
+			System.out.println("nuevo file");
+		}
+	}
+	
+	public static void updateFile(String fileName) {
+	/*
+	 * Método para actualizar fichero
+	 */
+		Path path = Paths.get(workdir, fileName);
+		
+		file = new File(path.toString());
+		if(file.exists()) {
+			System.out.println("Introduzca el texto que desea añadir al fichero: ");
+			miniOption = capturaDatos.next();
+			// añadir al final del fichero el texto
+			//System.out.println("Documento editado correctamente.");
+		} else {
+			System.out.println("Fichero inexistente");
+			capturaDatos.nextLine();
+			cleanConsole();
+		}
+	}
+	
+	public static void deleteFile(String fileName) {
+	/*
+	 * Método para borrar fichero
+	 */
+		Path path = Paths.get(workdir, fileName);
+		
+		file = new File(path.toString());
+		if(file.exists()) {
+			System.out.println("¿Estás seguro de que deseas borrarlo (Afirmación S)?");
+			try {
+				miniOption = capturaDatos.next("(S|s)$");
+				if(miniOption.contentEquals("S")) {
+					System.out.println(miniOption);
+					// eliminar fichero
+				} else {
+					System.out.println("El fichero no se borró del sistema.");
+					capturaDatos.nextLine();
+					cleanConsole();
+				}	
+			} catch (Exception e) { //Capturamos excepción por si el usuario ingresa cualquier cosa
+				System.out.println("El fichero no se borró del sistema.");
+				capturaDatos.nextLine();
+				cleanConsole();
+			}
+		} else {
+			System.out.println("Fichero inexistente");
+			capturaDatos.nextLine();
+			cleanConsole();
+		}
 	}
 }
