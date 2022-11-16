88b4fd3baba51a6c08449f440fd76ba07a9d6265
Waldo Araque <46325891+waldoaraque@users.noreply.github.com>
2022-11-16 20:19:20 +0100

    adding header comment

--- reto_4/src/Start.java
+++ reto_4/src/Start.java
@@ -1,3 +1,32 @@
+/******************************************************************************
+                    IFP Desarrollo de Aplicaciones Web
+                    1er Ciclo - Programación A
+                            Waldo Araque
+                            
+               Reto 4 - Programación Orientada a Objetos
+							 
+							 16/11/2022
+*******************************************************************************/
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Familia araqueFamily = null;
+		Persona hermana = null;
+		Persona hermano = null;
+		Persona padre = null;
+		
+		hermana = new Persona("Ludy Araque", 43, "Femenino", 57.0f, 1.63f);
+		hermano = new Persona("Willmer Araque", 39, "Masculino", 73.0f, 1.72f);
+		padre = new Persona("Gregorio Araque", 72, "Masculino", 70.0f, 1.67f);
+		
+		araqueFamily = new Familia(17893, "Av. Reyes Católicos, 14, 3A", "Cuenca", "Cuenca", padre, hermano, hermana);
+		System.out.println(padre.getPersonaInfo() + "El peso ideal es: "+ araqueFamily.imc(padre) + "\n");
+		System.out.println(hermana.getPersonaInfo() + "El peso ideal es: " + araqueFamily.imc(hermana) + "\n");
+		System.out.println(hermano.getPersonaInfo() + "El peso ideal es: "+ araqueFamily.imc(hermano) + "\n");
+	}
+
+}
 
 public class Start {
 
